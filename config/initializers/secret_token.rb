daa2050369d8acf19e12a423e15e89bccc91b831
Alex Burkhart <saterus@gmail.com>
2013-02-16 12:52:25 -0500

    Generate new rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suggestotron::Application.config.secret_token = 'e5bed3a5204d558582f86432af4639cd711543cf5bb940c1de442f0f5550c1318110828221d25137e9ef67977f4a31f0e05e6bc4d71bdba1726caa30177fa31d'
