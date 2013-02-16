0b4110ac8abf4206b03a719f506c8042a1a1c3a2
Alex Burkhart <saterus@gmail.com>
2013-02-16 15:39:27 -0500

    Remove annoying index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Suggestotron::Application.routes.draw do
+
   resources :votes
-
-
   resources :topics
 
 
@@ -61,4 +60,17 @@ Suggestotron::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  root :to => "topics#index"
+
+
+
+
+
+
+
+
+
+
+
 end
